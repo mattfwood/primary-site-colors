1bf9489c228d4ab8104b6a540ac549239dc2f950
mwood <matt@cratebind.com>
2018-04-25 15:36:06 -0500

    added puppeteer flags

--- getScreenshot.js
+++ getScreenshot.js
@@ -2,7 +2,9 @@ const puppeteer = require('puppeteer');
 
 async function getScreenshot(url, fileName, viewport) {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
     const page = await browser.newPage();
     await page.goto(url);
     await page.setViewport(viewport);
